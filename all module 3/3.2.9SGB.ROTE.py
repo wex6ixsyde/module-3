51d6a9d04ebd5be503475a413b5800f10ef09361
wex <seanrote85@gmail.com>
2025-09-29 09:13:01 +0800

    3.2.9.py

--- all module 3/3.2.9SGB.ROTE.py
+++ all module 3/3.2.9SGB.ROTE.py
@@ -18,7 +18,7 @@ Don't print any of the words entered by the user. Use the concept of conditional
 # solution 1
 """
 word = "" #gumamit po ako ng empty string para ma-initialize yung variable na word
-while word != "chupacabra": ##gumamit po ako != para sa condition ng while loop para po maging infinite loop
+while word != "chupacabra": #gumamit po ako != para sa condition ng while loop para po maging infinite loop
     word = input("Enter a word: ")
     if word == "chupacabra": 
         print("You've successfully left the loop.") 
@@ -32,4 +32,5 @@ while True: #sa solution 2 po, gumamit po ako ng True kasi infinite loop din po 
     if word == "chupacabra":
         print("You've successfully left the loop.")
         break
+
 """
